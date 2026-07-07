368871f33fa48bacd047d6d34b84c6fb3e8b927f
Pankaj39 <44633641+Pankaj39@users.noreply.github.com>
2026-07-07 12:16:33 +0530

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,12 @@
-const menu=document.querySelector(".menu-toggle");
-const nav=document.querySelector(".navbar ul");
+const menu = document.querySelector(".menu-toggle");
+const nav = document.querySelector(".navbar ul");
 
-menu.addEventListener("click",()=>{
+menu.addEventListener("click", () => {
+    nav.classList.toggle("active");
+});
 
-nav.classList.toggle("active");
-
+document.querySelectorAll(".navbar ul li a").forEach(link => {
+    link.addEventListener("click", () => {
+        nav.classList.remove("active");
+    });
 });
